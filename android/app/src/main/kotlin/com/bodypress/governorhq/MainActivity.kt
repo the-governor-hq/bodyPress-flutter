8b4716339bdf357eb04f55acef59a60e0f93e5b9
youssef <youssef@ctd.ma>
2026-02-28 17:47:06 -0500

    refactor: update package names and identifiers to match new application structure

---
+++ android/app/src/main/kotlin/com/bodypress/governorhq/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bodypress.governorhq
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/bodypress_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bodypress_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
